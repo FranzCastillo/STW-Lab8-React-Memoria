9119f327717cb346cabc7a291182b452229279b4
Francisco Castillo <cas21562@uvg.edu.gt>
2023-04-22 21:49:41 -0600

    Screen Display Changed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import DS from "./components/DS/DS";
 const App = () => {
     return (
         <div className="App">
-            <DS />
+            <div className="left-column">
+                <h1>New Super Mario Bros DS</h1>
+                <h2>Memory Match</h2>
+                <button id="start-button">Start</button>
+            </div>
+            <div className="right-column">
+                <DS />
+            </div>
         </div>
+
     );
 };
 
